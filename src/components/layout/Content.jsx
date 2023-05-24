aadcacc904c7281a45d99d5493ad57070ae758c4
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2023-05-24 19:21:58 -0300

    fim projetinho

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import './Content.css'
 import About from '../../views/examples/About'
 import Home from '../../views/examples/Home'
+import Param from '../../views/examples/Param'
 import { Routes, Route } from "react-router-dom"
+import NotFound from "../../views/examples/NotFound";
 
 const Content = props => (
     <main className="Content">
        <Routes>
-            <Route path="/about" element={<About/>} />
             <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/param/:id" element={<Param/>} />
+            <Route path="*" element={<NotFound/>} />
        </Routes>
     </main>
 )
